09d86a6e07363e42acd520eb947a7afea9177b41
Swarup Donepudi <swarup.donepudi@gmail.com>
2026-05-14 04:43:44 +0530

    removed company name from mobile header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -48,7 +48,7 @@ function Header({ className, ...props }: HeaderProps) {
       >
         <div className="h-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="h-full flex items-center justify-between">
-            <Logo size="md" />
+            <Logo size="md" showText={false} />
 
             <nav
               className="hidden md:flex items-center gap-1"
